fa2a79cc591b047c7b28ba8b2e1e0d986a39d765
veganum <veganum@hotmail.com>
2021-03-14 23:51:59 +0100

    probando

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
 
 export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+  return (
+    <h1 className="products">
+      <iframe
+        allow="microphone;"
+        width="350"
+        height="430"
+        src="https://console.dialogflow.com/api-client/demo/embedded/a46b7614-b98e-492e-b04b-919a6e838aff"
+      ></iframe>
+    </h1>
+  );
 }
