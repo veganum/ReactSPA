976639a37dfa3fc80a042893f5b2fadab3358445
veganum <veganum@hotmail.com>
2021-03-15 16:31:26 +0100

    Agregando componente history

---
+++ src/components/History.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../components/History.css";
+
+function History(props) {
+  return (
+    <>
+      <div class="history">
+        <div class="flex-container">
+          <div class="historyLeft">
+            <h1 class="title">Mi historia</h1>
+            <p class="text">
+              Soy un Antropólogo que ha decidido dar un giro a su carrera,
+              orientándome hacia el sector de las TIC y el desarrollo de
+              aplicaciones.
+            </p>
+          </div>
+          <div class="historyRight">
+            <img className="img" src="images/fotoJose.jpg" />
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
+
+export default History;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import '../../App.css';
-import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+import Footer from "../Footer";
+import History from "../History";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <History />
       <Cards />
       <Footer />
     </>
