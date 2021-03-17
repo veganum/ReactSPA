9a6ee79f54a6b90d41afe87715efa4098dfe6115
veganum <veganum@hotmail.com>
2021-03-17 01:09:57 +0100

    puliendo history componet

--- src/components/History.js
+++ src/components/History.js
@@ -5,22 +5,19 @@ import "../components/History.css";
 function History(props) {
   return (
     <>
-      <div class="history">
-        <div class="flex-container">
-          <div class="historyLeft">
-            <h1 class="title">Mi historia</h1>
-            <p class="text">
-              Soy un Antropólogo que ha decidido dar un giro a su carrera,
-              orientándome hacia el sector de las TIC y el desarrollo de
-              aplicaciones.
-            </p>
-          </div>
-          <div class="historyRight">
-            <img className="img" alt="mi imagen" src="images/fotoJose.jpg" />
-          </div>
+      <div class="flex-container">
+        <div class="textoContenedor">
+          <h1 class="titulo">Mi historia</h1>
+          <p class="texto">
+            Soy un Antropólogo que ha decidido dar un giro a su carrera,
+            orientándome hacia el sector de las TIC y el desarrollo de
+            aplicaciones.
+          </p>
+        </div>
+        <div class="imagenContenedor">
+          <img class="imagen" alt="mi imagen" src="images/fotoJose.jpg" />
         </div>
       </div>
-      <hr />
     </>
   );
 }
